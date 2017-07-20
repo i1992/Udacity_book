112cd7ef5464fc75bd6c8926c0e223bfa9a5646f
Hitarth shah <jay_shree_krishna@Hitarths-MacBook-Pro.local>
2017-07-19 17:09:49 -0700

    first

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import * as BooksAPI from './BooksAPI'
+import Getbook from './Getbook'
+import { Route } from 'react-router-dom'
+import Search from './Search'
+import './App.css'
+
+class BooksApp extends Component {
+  state = {
+    book : []
+
+  }
+
+  // componentDidMount(){
+  //   BooksAPI.search('art').then((book)=>{
+  //     this.setState({ book })
+  //
+  //     console.log(book);
+  //   })
+  // }
+
+  // searchbook(query){
+  //   BooksAPI.search(query).then(query =>{
+  //     this.setState(state => ({
+  //       book : state.book.filter([books])
+  //     }))
+  //   })
+  // }
+
+  render() {
+    return (
+      <div className="app">
+
+        <Route exact path='/' component={Getbook}/>
+        <Route path='/search' render={()=>(
+          <Search book = {this.state.book}
+          />
+        )}/>
+
+      </div>
+    )
+  }
+}
+
+export default BooksApp
